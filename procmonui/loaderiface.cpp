e47d62fc653245a917f1b933a98a32b93f6fb7e5
Alexander Nestorov <alexandernst@gmail.com>
2013-06-01 11:55:13 +0200

    Stop using Qt. I'm switching to text interface, mainly because of servers running without X

--- procmonui/loaderiface.cpp
+++
@@ -1,8 +0,0 @@
-#include "loaderiface.h"
-
-LoaderIface::LoaderIface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent) : QDBusAbstractInterface(service, path, staticInterfaceName(), connection, parent){
-}
-
-LoaderIface::~LoaderIface(){
-}
-
