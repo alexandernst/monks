8e3f961d94cf3fc57b95d05978e05e02489f9ca7
Alexander Nestorov <alexandernst@gmail.com>
2013-06-02 02:02:57 +0200

    Basic example. Load -> Start -> Stop -> Unload

--- procmon.rb
+++ procmon.rb
@@ -4,6 +4,11 @@ include LKM
 if LKM::check('procmon')
 	puts "Procmon's kernel module doesn't seem to be loaded."
 	LKM::load('./procmon_kmodule/procmon.ko')
+	sleep(2)
+	LKM::start()
+	sleep(2)
+	LKM::stop()
+	sleep(2)
 	LKM::unload('procmon')
 end
 
