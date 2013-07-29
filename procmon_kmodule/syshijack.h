0f5b90b9d998d1d3cccc63646f32278c4ae5fff3
Alexander Nestorov <alexandernst@gmail.com>
2013-07-30 00:52:04 +0200

    Move /proc related stuff to control.c

--- procmon_kmodule/syshijack.h
+++ procmon_kmodule/syshijack.h
@@ -148,6 +148,21 @@ void hook_calls(void);
 void unhook_calls(void);
 
 /*****************************************************************************\
+| Control                                                                     |
+\*****************************************************************************/
+
+extern char proc_data[1];
+extern struct proc_dir_entry *proc_write_entry;
+extern const struct file_operations proc_file_fops;
+ssize_t read_proc(struct file *file, char __user *buf, size_t count, loff_t *pos);
+ssize_t write_proc(struct file *file, const char __user *buf, size_t count, loff_t *pos);
+
+/*****************************************************************************\
+|                                      END                                    |
+\*****************************************************************************/
+
+
+/*****************************************************************************\
 | Utils                                                                       |
 \*****************************************************************************/
 
