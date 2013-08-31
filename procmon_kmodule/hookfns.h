1da0817eb26d6a4672a3d6989813eb0d1b224bc5
Alexander Nestorov <alexandernst@gmail.com>
2013-08-31 03:53:49 +0200

    Intent using tabs

--- procmon_kmodule/hookfns.h
+++ procmon_kmodule/hookfns.h
@@ -11,46 +11,46 @@
 | FF = FAKE FUNCTION as in the function which we'll be using to fake F        |
 \*****************************************************************************/
 
-#define __HOOK(F, RF, FF)			\
-do {						\
-	DEBUG(KERN_INFO "HOOK " #F "\n");	\
-	RF = (void *)sys_call_table[F];		\
-	sys_call_table[F] = (void *)FF;		\
-	REGISTER(F);				\
-} while(0)
+#define __HOOK(F, RF, FF)									\
+do{															\
+	DEBUG(KERN_INFO "HOOK " #F "\n");						\
+	RF = (void *)sys_call_table[F];							\
+	sys_call_table[F] = (void *)FF;							\
+	REGISTER(F);											\
+}while(0)
 
-#define HOOK(name)				\
+#define HOOK(name)											\
 	__HOOK(__NR_##name, real_sys_##name, hooked_sys_##name)
 
-#define __UNHOOK(F, RF)				\
-do {						\
-	DEBUG(KERN_INFO "UNHOOK " #F "\n");	\
-	sys_call_table[F] = (void *)RF;		\
-} while (0)
+#define __UNHOOK(F, RF)										\
+do{															\
+	DEBUG(KERN_INFO "UNHOOK " #F "\n");						\
+	sys_call_table[F] = (void *)RF;							\
+}while(0)
 
-#define UNHOOK(name)				\
+#define UNHOOK(name)										\
 	__UNHOOK(__NR_##name, real_sys_##name)
 
 #ifdef CONFIG_IA32_EMULATION
 
-#define __HOOK_IA32(F, RF, FF)			\
-do {						\
-	DEBUG(KERN_INFO "HOOK_IA32 " #F "\n");	\
-	RF = (void *)ia32_sys_call_table[F];	\
-	ia32_sys_call_table[F] = (void *)FF;	\
-	REGISTER(F);				\
-} while (0)
+#define __HOOK_IA32(F, RF, FF)								\
+do{															\
+	DEBUG(KERN_INFO "HOOK_IA32 " #F "\n");					\
+	RF = (void *)ia32_sys_call_table[F];					\
+	ia32_sys_call_table[F] = (void *)FF;					\
+	REGISTER(F);											\
+}while(0)
 
-#define HOOK_IA32(name)				\
+#define HOOK_IA32(name)										\
 	__HOOK_IA32(__NR32_##name, real_sys32_##name, hooked_sys32_##name)
 
-#define __UNHOOK_IA32(F, RF)			\
-do {						\
-	DEBUG(KERN_INFO "UNHOOK_IA32 " #F "\n");\
-	ia32_sys_call_table[F] = (void *)RF;	\
-} while (0)
+#define __UNHOOK_IA32(F, RF)								\
+do{															\
+	DEBUG(KERN_INFO "UNHOOK_IA32 " #F "\n");				\
+	ia32_sys_call_table[F] = (void *)RF;					\
+}while(0)
 
-#define UNHOOK_IA32(name)			\
+#define UNHOOK_IA32(name)									\
 	__UNHOOK_IA32(__NR32_##name, real_sys32_##name)
 
 #endif /* CONFIG_IA32_EMULATION */
--- procmon_kmodule/syscalls_hash.h
+++ procmon_kmodule/syscalls_hash.h
@@ -11,41 +11,47 @@
 struct syscall_hash {
 	char name[256];
 	unsigned long int n_calls;
-	UT_hash_handle hh;                                       
+	UT_hash_handle hh;
 };
 
 extern struct syscall_hash *syscall_items;
 
-#define REGISTER(F)                                         \
-for(int i = 0; i < 1; i++){                                 \
-	struct syscall_hash *syscall =                          \
-		(struct syscall_hash*)                              \
-		kmalloc(sizeof(struct syscall_hash), GFP_KERNEL);   \
-	strncpy(syscall->name, #F, 256);                        \
-	syscall->n_calls = 0;                                   \
-	HASH_ADD_STR(syscall_items, name, syscall);             \
-}
+#define REGISTER(F)											\
+do{															\
+	struct syscall_hash *syscall = 							\
+		(struct syscall_hash*)								\
+		kmalloc(sizeof(struct syscall_hash), GFP_KERNEL);	\
+	strncpy(syscall->name, #F, 256);						\
+	syscall->n_calls = 0;									\
+	HASH_ADD_STR(syscall_items, name, syscall);				\
+}while(0)
 
-#define INCR_SYSCALL_REG_INFO(F)                            \
-struct syscall_hash *item;                                  \
-HASH_FIND_STR(syscall_items, #F, item);                     \
-if(item){                                                   \
-	item->n_calls++;                                        \
-}
+#define INCR_SYSCALL_REG_INFO(F)							\
+do{															\
+	struct syscall_hash *item;								\
+	HASH_FIND_STR(syscall_items, #F, item);					\
+	if(item){												\
+		item->n_calls++;									\
+	}														\
+}while(0)
 
-#define DECR_SYSCALL_REG_INFO(F)                            \
-struct syscall_hash *item;                                  \
-HASH_FIND_STR(syscall_items, #F, item);                     \
-if(item){                                                   \
-	item->n_calls--;                                        \
-}
+#define DECR_SYSCALL_REG_INFO(F)							\
+do{															\
+	struct syscall_hash *item;								\
+	HASH_FIND_STR(syscall_items, #F, item);					\
+	if(item){												\
+		item->n_calls--;									\
+	}														\
+}while(0)
 
-#define SHOW_SYSCALL_REG_INFO(F)                            \
-struct syscall_hash *item, *tmp;                            \
-HASH_ITER(hh, syscall_items, item, tmp){                    \
-	printk(KERN_INFO "Name: %s", item->name);               \
-	printk(KERN_INFO "N calls/sec: %lu\n", item->n_calls);  \
-}
+#define SHOW_SYSCALL_REG_INFO(F)							\
+do{															\
+	struct syscall_hash *item, *tmp;						\
+	HASH_ITER(hh, syscall_items, item, tmp){				\
+		printk(KERN_INFO "Name: %s", item->name);			\
+		printk(KERN_INFO "Calls/sec: %lu\n", item->n_calls);\
+	}														\
+}while(0)
 
 /*****************************************************************************\
 |                                      END                                    |
