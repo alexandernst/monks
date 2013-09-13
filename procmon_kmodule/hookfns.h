ea943bb47e17c2e30e644b10577816bc7f466ec6
Alexander Nestorov <alexandernst@gmail.com>
2013-09-14 01:07:22 +0200

    Make the (un)hook(32) macros more readable

--- procmon_kmodule/hookfns.h
+++ procmon_kmodule/hookfns.h
@@ -11,67 +11,53 @@ typedef struct counter_info {
 extern counter_info_t __start_counters[];
 extern counter_info_t __stop_counters[];
 
-/*****************************************************************************\
-| HOOK MACROS                                                                 |
-| F, RF and FF stand for:                                                     |
-| F = FUNCTION as defined in include/linux/syscalls.h                         |
-| RF = REAL FUNCTION as in the function in which we will save F               |
-| FF = FAKE FUNCTION as in the function which we'll be using to fake F        |
-\*****************************************************************************/
+/*****************************************************************************************\
+| HOOK MACROS                                                                             |
+| __NR_#F, real_sys_##F and hooked_sys_##F stand for:                                     |
+| __NR_#F = FUNCTION as defined in include/linux/syscalls.h                               |
+| real_sys_##F = REAL FUNCTION as in the function in which we will save __NR_##F          |
+| hooked_sys_##F = FAKE FUNCTION as in the function which we'll be using to fake __NR_##F |
+\*****************************************************************************************/
 
-#define __HOOK(F, RF, FF)									\
-	DEBUG(KERN_INFO "HOOK " #F "\n");						\
-	RF = (void *)sys_call_table[F];							\
-	sys_call_table[F] = (void *)FF;
+#define HOOK(F)													\
+	DEBUG(KERN_INFO "HOOK __NR_" #F "\n");						\
+	real_sys_##F = (void *)sys_call_table[__NR_##F];			\
+	sys_call_table[__NR_##F] = (void *)hooked_sys_##F;
 
-#define REGISTER_SYSCALL(F)									\
-	static counter_info_t __counter_info___NR_##F			\
-	__attribute((unused, section(".counters"))) = {			\
-		.counter = ATOMIC_INIT(0),							\
-		.name = "__NR_" #F,									\
+#define UNHOOK(F)												\
+	DEBUG(KERN_INFO "UNHOOK __NR_" #F "\n");					\
+	sys_call_table[__NR_##F] = (void *)real_sys_##F;
+
+#define REGISTER_SYSCALL(F)										\
+	static counter_info_t __counter_info___NR_##F				\
+	__attribute((unused, section(".counters"))) = {				\
+		.counter = ATOMIC_INIT(0),								\
+		.name = "__NR_" #F,										\
 	};
-
-#define HOOK(name)											\
-	__HOOK(__NR_##name, real_sys_##name, hooked_sys_##name);
-
-#define __UNHOOK(F, RF)										\
-	DEBUG(KERN_INFO "UNHOOK " #F "\n");						\
-	sys_call_table[F] = (void *)RF;
-
-#define UNHOOK(name)										\
-	__UNHOOK(__NR_##name, real_sys_##name)
-
-#ifdef CONFIG_IA32_EMULATION
-
-#define __HOOK_IA32(F, RF, FF)								\
-	DEBUG(KERN_INFO "HOOK_IA32 " #F "\n");					\
-	RF = (void *)ia32_sys_call_table[F];					\
-	ia32_sys_call_table[F] = (void *)FF;
-
-#define REGISTER_SYSCALL32(F)								\
-	static counter_info_t __counter_info___NR32_##F			\
-	__attribute((unused, section(".counters"))) = {			\
-		.counter = ATOMIC_INIT(0),							\
-		.name = "__NR32_" #F "_32",							\
-	};
-
-#define HOOK_IA32(name)										\
-	__HOOK_IA32(__NR32_##name, real_sys32_##name, hooked_sys32_##name);
-
-#define __UNHOOK_IA32(F, RF)								\
-	DEBUG(KERN_INFO "UNHOOK_IA32 " #F "\n");				\
-	ia32_sys_call_table[F] = (void *)RF;
-
-#define UNHOOK_IA32(name)									\
-	__UNHOOK_IA32(__NR32_##name, real_sys32_##name)
-
-
 
 #define __INCR(F)	\
 	atomic_inc(&__counter_info___NR_##F.counter);
 
 #define __DECR(F)	\
 	atomic_dec(&__counter_info___NR_##F.counter);
+
+#ifdef CONFIG_IA32_EMULATION
+
+#define HOOK_IA32(F)											\
+	DEBUG(KERN_INFO "HOOK_IA32 __NR32_" #F "\n");				\
+	real_sys32_##F = (void *)ia32_sys_call_table[__NR32_##F];	\
+	ia32_sys_call_table[__NR32_##F] = (void *)hooked_sys32_##F;
+
+#define UNHOOK_IA32(F)											\
+	DEBUG(KERN_INFO "UNHOOK_IA32 __NR32_" #F "\n");				\
+	ia32_sys_call_table[__NR32_##F] = (void *)real_sys32_##F;
+
+#define REGISTER_SYSCALL32(F)									\
+	static counter_info_t __counter_info___NR32_##F				\
+	__attribute((unused, section(".counters"))) = {				\
+		.counter = ATOMIC_INIT(0),								\
+		.name = "__NR32_" #F "_32",								\
+	};
 
 #define __INCR32(F)	\
 	atomic_inc(&__counter_info___NR32_##F.counter);
