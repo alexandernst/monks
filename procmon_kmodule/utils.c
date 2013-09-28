2b96ac13f82183c68b70c612ccdc2e68617d560b
Alexander Nestorov <alexandernst@gmail.com>
2013-09-28 03:26:31 +0200

    Stop printk-ing messages, send them instead to userland via NetLink and how them there

--- procmon_kmodule/utils.c
+++ procmon_kmodule/utils.c
@@ -1,21 +1,60 @@
 #include "utils.h"
 
-void print_info(syscall_info *i){
-	printk(KERN_INFO
-		"%-15.10s"       /* Process name */
-		"%10u"           /* PID          */
-		" %-15.10s"      /* Operation    */
-		"%-50.45s"       /* Path         */
-		"%-10.10s"       /* Result       */
-		" %-200.200s\n", /* Details      */
+#define NETLINK_USER 31
+int client_pid = 0;
+struct sock *nl_sk = NULL;
 
-		i->pname,
-		i->pid,
-		i->operation,
-		i->path,
-		i->result,
-		i->details
-	);
+void nl_init(void){
+	/* This is for 3.6 kernels and above.*/
+	struct netlink_kernel_cfg cfg = {
+		.input = nl_recv,
+	};
+
+	nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
+	/*Bellow kernel 3.6*/
+	//nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, 0, nl_recv, NULL, THIS_MODULE);
+	
+	if(!nl_sk){
+		DEBUG(KERN_INFO "Error creating socket.\n");
+	}
+}
+
+void nl_halt(void){
+	netlink_kernel_release(nl_sk);
+}
+
+void nl_recv(struct sk_buff *skb){
+	struct nlmsghdr *nlh;
+
+	nlh = (struct nlmsghdr*)skb->data;
+	client_pid = nlh->nlmsg_pid;
+}
+
+void nl_send(syscall_info *i){
+	struct nlmsghdr *nlh;
+	struct sk_buff *skb_out;
+	int msg_size;
+
+	membuffer *x = serialize_syscall_info(i);
+	byte *data = x->data;
+
+	char *q = serialize_membuffer(x);
+	msg_size = sizeof(size_t) + x->len;
+
+	skb_out = nlmsg_new(msg_size, 0);
+
+	if(!skb_out){
+		return;
+	} 
+	nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);  
+	NETLINK_CB(skb_out).dst_group = 0;
+	memcpy(nlmsg_data(nlh), q, msg_size);
+
+	nlmsg_unicast(nl_sk, skb_out, client_pid);
+
+	kfree(data);
+	kfree(x);
+	kfree(q);
 }
 
 char *path_from_fd(unsigned int fd){
--- procmon_kmodule/utils.h
+++ procmon_kmodule/utils.h
@@ -1,22 +1,34 @@
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
 
-#include "main.h"
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fdtable.h>
+#include <linux/skbuff.h>
+#include <linux/netlink.h>
+
+#include <net/sock.h>
+
+#include "../common/structures.h"
+#include "../common/serialize.h"
 
 /*****************************************************************************\
 | Utils                                                                       |
 \*****************************************************************************/
 
-typedef struct syscall_intercept_info{
-	char *pname;
-	pid_t pid;
-	char *operation;
-	char *path;
-	char *result;
-	char *details;
-} syscall_info;
+#define debug 1
 
-void print_info(syscall_info *i);
+#if debug == 1
+#define DEBUG(...) printk(__VA_ARGS__);
+#else
+#define DEBUG(...)
+#endif
+
+void nl_init(void);
+void nl_halt(void);
+void nl_recv(struct sk_buff *skb);
+void nl_send(syscall_info *i);
+
 char *path_from_fd(unsigned int fd);
 
 /*****************************************************************************\
