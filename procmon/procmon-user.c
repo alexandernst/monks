966a349dea1762893ebe22b73e9bb3eaa6101a55
Alexander Nestorov <alexandernst@gmail.com>
2013-09-28 03:48:09 +0200

    Add userland code to read the NetLink messages from the LKM

---
+++ procmon/procmon-user.c
@@ -0,0 +1,87 @@
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "../common/structures.h"
+#include "../common/deserialize.h"
+
+void print_info(syscall_info *i);
+
+#define NETLINK_USER 31
+
+#define MAX_PAYLOAD 1024
+struct sockaddr_nl src_addr, dest_addr;
+struct nlmsghdr *nlh = NULL;
+struct iovec iov;
+int sock_fd;
+struct msghdr msg;
+
+int main(){
+	sock_fd=socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+	if(sock_fd<0)
+	return -1;
+
+	memset(&src_addr, 0, sizeof(src_addr));
+	src_addr.nl_family = AF_NETLINK;
+	src_addr.nl_pid = getpid();
+
+	bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
+
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	dest_addr.nl_family = AF_NETLINK;
+	dest_addr.nl_pid = 0;
+	dest_addr.nl_groups = 0;
+
+	nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+	memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+	nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+	nlh->nlmsg_pid = getpid();
+	nlh->nlmsg_flags = 0;
+
+	strcpy(NLMSG_DATA(nlh), "");
+
+	iov.iov_base = (void *)nlh;
+	iov.iov_len = nlh->nlmsg_len;
+	msg.msg_name = (void *)&dest_addr;
+	msg.msg_namelen = sizeof(dest_addr);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	sendmsg(sock_fd,&msg,0);
+	printf("Waiting for message from kernel\n");
+
+	while(1){
+		recvmsg(sock_fd, &msg, 0);
+
+		char *q = (char *)NLMSG_DATA(nlh);
+		membuffer *x = deserialize_membuffer(q);
+		syscall_info *i = deserialize_syscall_info(x);
+
+		print_info(i);
+
+		free(x);
+		free(i);
+	}
+	close(sock_fd);
+}
+
+void print_info(syscall_info *i){
+	printf(
+		"%-15.10s"       /* Process name */
+		"%10u"           /* PID          */
+		" %-15.10s"      /* Operation    */
+		"%-50.45s"       /* Path         */
+		"%-10.10s"       /* Result       */
+		" %-200.200s\n", /* Details      */
+
+		i->pname,
+		i->pid,
+		i->operation,
+		i->path,
+		i->result,
+		i->details
+	);
+}
