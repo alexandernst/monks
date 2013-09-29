157215feeaabe055150c020b2c125b20de9db713
Alexander Nestorov <alexandernst@gmail.com>
2013-09-29 20:58:54 +0200

    Add lib for choosing between k(malloc,realloc,free) when needed

---
+++ common/mem_ops.h
@@ -0,0 +1,17 @@
+#ifndef MEM_OPS_H_INCLUDED
+#define MEM_OPS_H_INCLUDED
+
+#ifdef __KERNEL__
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#else
+#include <stdlib.h>
+#include <stdio.h>
+#endif
+
+void *new(size_t sz);
+void *renew(void *ptr, size_t sz);
+void del(void *ptr);
+
+#endif
