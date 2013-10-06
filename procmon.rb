36daeb326b0ce35c1a6f250e43df7cc7b6039b6b
Alexander Nestorov <alexandernst@gmail.com>
2013-10-06 16:25:05 +0200

    Make the ruby script actually do something

--- procmon.rb
+++ procmon.rb
@@ -1,14 +1,27 @@
-require './procmon/lkm'
+require 'open3'
+require './procmon/ui/lkm'
 include LKM
 
 if LKM::check('procmon')
 	puts "Procmon's kernel module doesn't seem to be loaded."
-	LKM::load('./procmon_kmodule/procmon.ko')
-	sleep(2)
+	LKM::load('./procmon.ko')
 	LKM::start()
-	sleep(2)
+
+	puts "Starting procmon-viewer..."
+	output = []
+	i = 0
+	Open3.popen2("./procmon-viewer") do |stdin, stdout, wait_thr|
+		stdout.each do |line|
+			i += 1
+			output << line.chomp
+			if i > 500 then
+				break
+			end
+		end
+		%x(kill -9 #{wait_thr.pid})
+	end
+	puts "#{output.size} lines where catched by procmon-viewer."
+
 	LKM::stop()
-	sleep(2)
 	LKM::unload('procmon')
 end
-
