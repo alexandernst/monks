299a17079a5bc2e8de3b281c583a132232676b20
Ilya V. Matveychikov <matvejchikov@gmail.com>
2013-10-31 12:27:58 +0400

    Use separate kernel thread to handle netlink data

--- procmon_kmodule/utils.c
+++ procmon_kmodule/utils.c
@@ -1,8 +1,76 @@
-#include "utils.h"
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+
+#include "main.h"
 
 int nl_id = MAX_LINKS - 1;
 struct sock *nl_sk = NULL;
 extern unsigned int sent_msgs;
+
+/* kernel thread id */
+static struct task_struct * nl_thread = NULL;
+/* kernel thread waitqueue */
+static DECLARE_WAIT_QUEUE_HEAD(nl_wait);
+/* kernel thread data queue */
+static struct sk_buff_head nl_queue;
+
+static int nl_show_skb(struct sk_buff * skb)
+{
+	procmon_info("TODO: show the skb\n");
+
+	return 0;
+}
+
+static int nl_send_skb(struct sk_buff * skb)
+{
+	int client = get_client_pid();
+
+	/* TODO: make client_pid global and keep it uptodate */
+
+	if (is_active() && client > 0) {
+		nlmsg_unicast(nl_sk, skb, client);
+	} else {
+		if (printk_ratelimit()) {
+			nl_show_skb(skb);
+		} else {
+			procmon_warning("printk limit exceeded\n");
+		}
+		kfree_skb(skb);
+	}
+
+	return 0;
+}
+
+static int nl_kernel_thread(void * arg)
+{
+	DECLARE_WAITQUEUE(wait, current);
+
+	add_wait_queue(&nl_wait, &wait);
+
+	while (!kthread_should_stop()) {
+		struct sk_buff * skb;
+
+		current->state = TASK_RUNNING;
+
+		while ((skb = skb_dequeue(&nl_queue)) != NULL) {
+			nl_send_skb(skb);
+			sent_msgs++;
+		}
+
+		if (signal_pending(current))
+			break;
+
+		current->state = TASK_INTERRUPTIBLE;
+
+		schedule();
+	}
+
+	remove_wait_queue(&nl_wait, &wait);
+
+	skb_queue_purge(&nl_queue);
+
+	return 0;
+}
 
 static struct sock *nl_init_sock(int netlink_id){
 	struct sock * nl_sk;
@@ -15,6 +83,15 @@ static struct sock *nl_init_sock(int netlink_id){
 }
 
 void nl_init(void){
+
+	skb_queue_head_init(&nl_queue);
+
+	nl_thread = kthread_run(nl_kernel_thread, NULL, "kpmnld");
+	if (IS_ERR_OR_NULL(nl_thread)) {
+		procmon_error("Can't create netlink thread\n");
+		return;
+	}
+
 	while (nl_id >= 0) {
 		if ((nl_sk = nl_init_sock(nl_id))) {
 			DEBUG(KERN_INFO "Acquired NETLINK socket (%d)\n", nl_id);
@@ -23,24 +100,21 @@ void nl_init(void){
 		nl_id--;
 	}
 
+	kthread_stop(nl_thread), nl_thread = NULL;
+
 	DEBUG(KERN_INFO "Error creating socket.\n");
 }
 
 void nl_halt(void){
 	netlink_kernel_release(nl_sk);
+
+	kthread_stop(nl_thread), nl_thread = NULL;
 }
 
 void nl_send(syscall_info *i){
-	int _client_pid;
 	membuffer *x;
 	struct nlmsghdr *nlh;
 	struct sk_buff *skb_out;
-
-	/*If there isn't any client listening, don't try sending anything*/
-	_client_pid = get_client_pid();
-	if(_client_pid < 0){
-		return;
-	}
 
 	x = serialize_syscall_info(i);
 	if(!x) return;
@@ -51,9 +125,9 @@ void nl_send(syscall_info *i){
 	nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, x->len, 0);  
 	NETLINK_CB(skb_out).dst_group = 0;
 	memcpy(nlmsg_data(nlh), x->data, x->len);
-	nlmsg_unicast(nl_sk, skb_out, _client_pid);
 
-	sent_msgs++;
+	skb_queue_tail(&nl_queue, skb_out);
+	wake_up_interruptible(&nl_wait);
 
 end:
 	del(x->data);
