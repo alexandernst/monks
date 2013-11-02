6925b4d05836a1539e198f2589dc59a34da208fd
Alexander Nestorov <alexandernst@gmail.com>
2013-11-02 15:37:25 +0100

    Some style changes

--- procmon_kmodule/utils.c
+++ procmon_kmodule/utils.c
@@ -4,31 +4,29 @@ int nl_id = MAX_LINKS - 1;
 struct sock *nl_sk = NULL;
 
 /* kernel thread id */
-static struct task_struct * nl_thread = NULL;
+static struct task_struct *nl_thread = NULL;
 /* kernel thread waitqueue */
 static DECLARE_WAIT_QUEUE_HEAD(nl_wait);
 /* kernel thread data queue */
 static struct sk_buff_head nl_queue;
 
-static int nl_show_skb(struct sk_buff * skb)
-{
+static int nl_show_skb(struct sk_buff *skb){
 	procmon_info("TODO: show the skb\n");
 
 	return 0;
 }
 
-static int nl_send_skb(struct sk_buff * skb)
-{
+static int nl_send_skb(struct sk_buff *skb){
 	int client = get_client_pid();
 
 	/* TODO: make client_pid global and keep it uptodate */
 
-	if (procmon_state && client > 0) {
+	if(procmon_state && client > 0){
 		nlmsg_unicast(nl_sk, skb, client);
-	} else {
-		if (printk_ratelimit()) {
+	}else{
+		if(printk_ratelimit()){
 			nl_show_skb(skb);
-		} else {
+		}else{
 			procmon_warning("printk limit exceeded\n");
 		}
 		kfree_skb(skb);
@@ -37,22 +35,21 @@ static int nl_send_skb(struct sk_buff * skb)
 	return 0;
 }
 
-static int nl_kernel_thread(void * arg)
-{
+static int nl_kernel_thread(void *arg){
 	DECLARE_WAITQUEUE(wait, current);
 
 	add_wait_queue(&nl_wait, &wait);
 
-	while (!kthread_should_stop()) {
-		struct sk_buff * skb;
+	while(!kthread_should_stop()){
+		struct sk_buff *skb;
 
 		current->state = TASK_RUNNING;
 
-		while ((skb = skb_dequeue(&nl_queue)) != NULL) {
+		while((skb = skb_dequeue(&nl_queue)) != NULL){
 			nl_send_skb(skb);
 		}
 
-		if (signal_pending(current))
+		if(signal_pending(current))
 			break;
 
 		current->state = TASK_INTERRUPTIBLE;
@@ -68,7 +65,7 @@ static int nl_kernel_thread(void * arg)
 }
 
 static struct sock *nl_init_sock(int netlink_id){
-	struct sock * nl_sk;
+	struct sock *nl_sk;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0)
 	nl_sk = netlink_kernel_create(&init_net, netlink_id, NULL);
 #else
@@ -78,7 +75,6 @@ static struct sock *nl_init_sock(int netlink_id){
 }
 
 void nl_init(void){
-
 	skb_queue_head_init(&nl_queue);
 
 	nl_thread = kthread_run(nl_kernel_thread, NULL, "kpmnld");
@@ -87,8 +83,8 @@ void nl_init(void){
 		return;
 	}
 
-	while (nl_id >= 0) {
-		if ((nl_sk = nl_init_sock(nl_id))) {
+	while(nl_id >= 0){
+		if((nl_sk = nl_init_sock(nl_id))){
 			procmon_info("Acquired NETLINK socket (%d)\n", nl_id);
 			return;
 		}
@@ -112,10 +108,12 @@ void nl_send(syscall_info *i){
 	struct sk_buff *skb_out;
 
 	x = serialize_syscall_info(i);
-	if(!x) return;
+	if(!x)
+		return;
 
 	skb_out = nlmsg_new(x->len, 0);
-	if(!skb_out) goto end;
+	if(!skb_out)
+		goto end;
 
 	nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, x->len, 0);  
 	NETLINK_CB(skb_out).dst_group = 0;
