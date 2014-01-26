792e852298247bc5bc363b6545f57e1b79b53d5e
Alexander Nestorov <alexandernst@gmail.com>
2014-01-26 19:37:06 +0100

    Clean __NR_read/32 interceptor code

--- monks_kmodule/syscalls/read.c
+++ monks_kmodule/syscalls/read.c
@@ -14,28 +14,30 @@ asmlinkage void hooked_sys_read(unsigned int fd, char __user *buf, size_t count)
 	__GET_SYSCALL_RESULT(r);
 
 	i = new(sizeof(struct syscall_intercept_info));
-	if(i){
-		i->pname = current->comm;
-		i->pid = current->pid;
-		i->operation = "READ";
-		i->path = path_from_fd(fd);
 
-		if(IS_ERR((void *)r)){
-			i->result = "Error";
-			i->details = kasprintf(GFP_KERNEL, "Errno %zd", r);
-		}else{
-			i->result = "Ok";
-			i->details = kasprintf(GFP_KERNEL, "Read %zd bytes (was requested to read %zd)", r, count);
-		}
+	if(!i)
+		return;
 
-		nl_send(i);
+	i->pname = current->comm;
+	i->pid = current->pid;
+	i->operation = "READ";
+	i->path = path_from_fd(fd);
 
-		del(i->path);
-		del(i->details);
-		del(i);
+	if(IS_ERR((void *)r)){
+		i->result = "Error";
+		i->details = kasprintf(GFP_KERNEL, "Errno %zd", r);
 	}else{
-		//something bad happened, can't show results
+		i->result = "Ok";
+		i->details = kasprintf(GFP_KERNEL, "Read %zd bytes (was requested to read %zd)", r, count);
 	}
+
+	nl_send(i);
+
+	del(i->path);
+	del(i->details);
+	del(i);
+
+	return;
 }
 
 #ifdef CONFIG_IA32_EMULATION
@@ -48,29 +50,31 @@ asmlinkage void hooked_sys32_read(unsigned int fd, char __user *buf, size_t coun
 	__GET_SYSCALL_RESULT32(r);
 
 	i = new(sizeof(struct syscall_intercept_info));
-	if(i){
-		i->pname = current->comm;
-		i->pid = current->pid;
-		i->operation = "READ32";
-		i->path = path_from_fd(fd);
 
-		if(IS_ERR((void *)r)){
-			i->result = "Error";
-			i->details = kasprintf(GFP_KERNEL, "Errno %zd", r);
-		}else{
-			i->result = "Ok";
-			i->details = kasprintf(GFP_KERNEL, "Read %zd bytes (was requested to read %zd)", r, count);
-		}
+	if(!i)
+		return;
 
-		if(count == 1 && fd == 0)
-			nl_send(i);
+	i->pname = current->comm;
+	i->pid = current->pid;
+	i->operation = "READ32";
+	i->path = path_from_fd(fd);
 
-		del(i->path);
-		del(i->details);
-		del(i);
+	if(IS_ERR((void *)r)){
+		i->result = "Error";
+		i->details = kasprintf(GFP_KERNEL, "Errno %zd", r);
 	}else{
-		//something bad happened, can't show results
+		i->result = "Ok";
+		i->details = kasprintf(GFP_KERNEL, "Read %zd bytes (was requested to read %zd)", r, count);
 	}
+
+	if(count == 1 && fd == 0)
+		nl_send(i);
+
+	del(i->path);
+	del(i->details);
+	del(i);
+
+	return;
 }
 
 #endif
