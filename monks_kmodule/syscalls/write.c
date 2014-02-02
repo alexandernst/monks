d732332cd2af3a95df4e37dabcfc2430a892fecc
Alexander Nestorov <alexandernst@gmail.com>
2014-02-02 01:34:45 +0100

    Convert write hook to pre/post

--- monks_kmodule/syscalls/write.c
+++ monks_kmodule/syscalls/write.c
@@ -1,19 +1,20 @@
 #include "syscall.h"
 
-extern asmlinkage void hooked_sys_write(unsigned int fd, char __user *buf, size_t count);
+extern asmlinkage void hooked_sys_pre_write(unsigned int fd, char __user *buf, size_t count);
+extern asmlinkage void hooked_sys_post_write(unsigned int fd, char __user *buf, size_t count);
 #ifdef CONFIG_IA32_EMULATION
-extern asmlinkage void hooked_sys32_write(unsigned int fd, char __user *buf, size_t count);
+extern asmlinkage void hooked_sys32_pre_write(unsigned int fd, char __user *buf, size_t count);
+extern asmlinkage void hooked_sys32_post_write(unsigned int fd, char __user *buf, size_t count);
 #endif
 
 __REGISTER_SYSCALL(write);
 
-asmlinkage void hooked_sys_write(unsigned int fd, char __user *buf, size_t count){
-	ssize_t r;
+asmlinkage void hooked_sys_pre_write(unsigned int fd, char __user *buf, size_t count){
 	syscall_intercept_info *i;
 
-	__GET_SYSCALL_RESULT(r);
-
 	i = new(sizeof(struct syscall_intercept_info));
+
+	__SET_SYSCALL_HOOK_INFO(i);
 
 	if(!i)
 		return;
@@ -22,6 +23,19 @@ asmlinkage void hooked_sys_write(unsigned int fd, char __user *buf, size_t count
 	i->pid = current->pid;
 	i->operation = "WRITE";
 	i->path = path_from_fd(fd);
+
+	return;
+}
+
+asmlinkage void hooked_sys_post_write(unsigned int fd, char __user *buf, size_t count){
+	ssize_t r;
+	syscall_intercept_info *i;
+
+	__GET_SYSCALL_RESULT(r);
+	__GET_SYSCALL_HOOK_INFO(i);
+
+	if(!i)
+		return;
 
 	if(IS_ERR((void *)r)){
 		i->result = "Error";
@@ -43,13 +57,12 @@ asmlinkage void hooked_sys_write(unsigned int fd, char __user *buf, size_t count
 #ifdef CONFIG_IA32_EMULATION
 __REGISTER_SYSCALL32(write);
 
-asmlinkage void hooked_sys32_write(unsigned int fd, char __user *buf, size_t count){
-	ssize_t r;
+asmlinkage void hooked_sys32_pre_write(unsigned int fd, char __user *buf, size_t count){
 	syscall_intercept_info *i;
 
-	__GET_SYSCALL_RESULT32(r);
-
 	i = new(sizeof(struct syscall_intercept_info));
+
+	__SET_SYSCALL_HOOK_INFO32(i);
 
 	if(!i)
 		return;
@@ -59,6 +72,19 @@ asmlinkage void hooked_sys32_write(unsigned int fd, char __user *buf, size_t cou
 	i->operation = "WRITE32";
 	i->path = path_from_fd(fd);
 
+	return;
+}
+
+asmlinkage void hooked_sys32_post_write(unsigned int fd, char __user *buf, size_t count){
+	ssize_t r;
+	syscall_intercept_info *i;
+
+	__GET_SYSCALL_RESULT32(r);
+	__GET_SYSCALL_HOOK_INFO32(i);
+
+	if(!i)
+		return;
+
 	if(IS_ERR((void *)r)){
 		i->result = "Error";
 		i->details = kasprintf(GFP_KERNEL, "Errno %zd", r);
@@ -67,8 +93,7 @@ asmlinkage void hooked_sys32_write(unsigned int fd, char __user *buf, size_t cou
 		i->details = kasprintf(GFP_KERNEL, "Write %zd bytes (was requested to write %zd)", r, count);
 	}
 
-	if(count == 1 && fd == 0)
-		nl_send(i);
+	nl_send(i);
 
 	del(i->path);
 	del(i->details);
